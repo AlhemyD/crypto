f46e89676e5f78302375401182e146512d1d4f4f
AlhemyD <alhemyd@mail.ru>
2025-03-19 14:33:29 +0800

    task done

---
+++ app/src/main/java/com/example/crypto/RateCheckInteractor.kt
@@ -0,0 +1,33 @@
+package com.example.crypto
+
+import android.util.Log
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import org.json.JSONObject
+
+class RateCheckInteractor {
+    val networkClient = NetworkClient()
+
+    suspend fun requestRate(): String {
+        return withContext(Dispatchers.IO) {
+            val result = networkClient.request(MainViewModel.USD_RATE_URL)
+            if (!result.isNullOrEmpty()) {
+                parseRate(result)
+            } else {
+                ""
+            }
+        }
+    }
+
+    private fun parseRate(jsonString: String): String {
+        try {
+            Log.d("RateCheckInteractorJSON", jsonString)
+            return JSONObject(jsonString)
+                .getString("RUB")
+        } catch (e: Exception) {
+
+            Log.e("RateCheckInteractor", "", e)
+        }
+        return ""
+    }
+}
---
+++ app/src/main/java/com/example/crypto/RateCheckService.kt
@@ -0,0 +1,73 @@
+package com.example.crypto
+
+import android.app.Service
+import android.content.Context
+import android.content.Intent
+import android.os.Handler
+import android.os.IBinder
+import android.os.Looper
+import android.util.Log
+import java.math.BigDecimal
+
+
+class RateCheckService : Service() {
+    val handler = Handler(Looper.getMainLooper())
+    lateinit var viewModel: MainViewModel
+    var rateCheckAttempt = 0
+    lateinit var startRate: BigDecimal
+    lateinit var targetRate: BigDecimal
+    val rateCheckInteractor = RateCheckInteractor()
+
+    val rateCheckRunnable: Runnable = Runnable {
+        Log.d("RateCheckRunnable: ", "rateCheckRunnable function started")
+        // Write your code here. Check number of attempts and stop service if needed
+        requestAndCheckRate()
+    }
+
+    private fun requestAndCheckRate() {
+        // Write your code here
+        Log.d("RequestCheckService: ", "checking service")
+        viewModel.onRefreshClicked()
+    }
+
+    override fun onBind(p0: Intent?): IBinder? = null
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        startRate = BigDecimal(intent?.getStringExtra(ARG_START_RATE))
+        targetRate = BigDecimal(intent?.getStringExtra(ARG_TARGET_RATE))
+
+        Log.d(TAG, "onStartCommand startRate = $startRate targetRate = $targetRate")
+
+        handler.post(rateCheckRunnable)
+
+        return START_STICKY
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        handler.removeCallbacks(rateCheckRunnable)
+    }
+
+
+    companion object {
+        const val TAG = "RateCheckService"
+        const val RATE_CHECK_INTERVAL = 5000L
+        const val RATE_CHECK_ATTEMPTS_MAX = 100
+
+        const val ARG_START_RATE = "ARG_START_RATE"
+        const val ARG_TARGET_RATE = "ARG_TARGET_RATE"
+
+        fun startService(context: Context, startRate: String, targetRate: String) {
+            Log.d("StartService: ", "startService function started")
+            context.startService(Intent(context, RateCheckService::class.java).apply {
+                putExtra(ARG_START_RATE, startRate)
+                putExtra(ARG_TARGET_RATE, targetRate)
+            })
+        }
+
+        fun stopService(context: Context) {
+            Log.d("StopService: ", "stopService function started")
+            context.stopService(Intent(context, RateCheckService::class.java))
+        }
+    }
+}
